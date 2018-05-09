353f70e38952fd1f68125093bff48c1c7bd3b3b4
gejian <pondcat@qq.com>
2018-05-09 23:24:36 +0800

    git init

---
+++ src/main/java/com/gj1913894/web/starter/WebServiceStarterApplication.java
@@ -0,0 +1,12 @@
+package com.gj1913894.web.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceStarterApplication.class, args);
+    }
+}
