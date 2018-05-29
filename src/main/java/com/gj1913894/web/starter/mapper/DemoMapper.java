86279a2a8dd29e97c303a2b707da481347a47f54
gejian <pondcat@qq.com>
2018-05-29 09:19:45 +0800

    增加mybatis-plus支持

--- src/main/java/com/gj1913894/web/starter/mapper/DemoMapper.java
+++ src/main/java/com/gj1913894/web/starter/mapper/DemoMapper.java
@@ -4,4 +4,5 @@ package com.gj1913894.web.starter.mapper;
  * @author gejian
  */
 public interface DemoMapper {
+	// todo 提供existsByXxx的示例
 }
--- src/main/java/com/gj1913894/web/starter/service/DemoService.java
+++ src/main/java/com/gj1913894/web/starter/service/DemoService.java
@@ -1,10 +1,15 @@
 package com.gj1913894.web.starter.service;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 
 /**
  * @author gejian
  */
-public interface DemoService {
-	LocalDateTime serve();
+@Service
+public class DemoService {
+	public LocalDateTime serve() {
+		return LocalDateTime.now();
+	}
 }
