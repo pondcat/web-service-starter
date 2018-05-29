a379ce24fca648ad2271d66c9f7e61ac4c1bc8f9
行一 <gejian@mogoroom.com>
2018-05-29 21:04:04 +0800

    切换mybatis-plus为mybatis-mapper, mybatis-plus的乐观锁插件写的一坨屎一样

--- src/main/java/com/gj1913894/web/starter/WebServiceStarterApplication.java
+++ src/main/java/com/gj1913894/web/starter/WebServiceStarterApplication.java
@@ -1,14 +1,11 @@
 package com.gj1913894.web.starter;
 
-import com.gj1913894.web.starter.mapper.DemoMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author gejian
  */
-@MapperScan(basePackageClasses = DemoMapper.class)
 @SpringBootApplication
 public class WebServiceStarterApplication {
 
