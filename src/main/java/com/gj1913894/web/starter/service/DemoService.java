9809d48ab79fd30df94d082ee73ba1de5bee165e
行一 <gejian@mogoroom.com>
2018-05-30 21:07:49 +0800

    tools

--- src/main/java/com/gj1913894/web/starter/service/DemoService.java
+++ src/main/java/com/gj1913894/web/starter/service/DemoService.java
@@ -1,13 +1,17 @@
 package com.gj1913894.web.starter.service;
 
+import com.gj1913894.web.starter.entity.DbEntity;
 import com.gj1913894.web.starter.entity.User;
 import com.gj1913894.web.starter.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.beans.BeanCopier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
 
 /**
  * @author gejian
@@ -16,9 +20,23 @@ import java.util.List;
 public class DemoService implements Serializable {
 	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") @Autowired private UserMapper userMapper;
 
+	@Transactional
 	public LocalDateTime serve() {
-		List<User> users = userMapper.selectAll();
-		System.out.println(users);
+		User user = new User();
+		user.setMobile("13712345678");
+		user.setRealName("lis");
+		user.setStat("normal");
+		userMapper.insert(user);
+		TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+			@Override
+			public void afterCommit() {
+				user.setId(null);
+				userMapper.insert(user);
+			}
+		});
+		BeanCopier beanCopier = BeanCopier.create(User.class, DbEntity.class, true);
+
 		return LocalDateTime.now();
 	}
+
 }
